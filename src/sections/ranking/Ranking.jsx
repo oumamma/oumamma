598faa0e25a2c9388e673748ae4d0ebf72e58069
Sergio Pérez Andrade <sergioperezandrade@MacBook-Pro-de-Sergio.local>
2019-10-21 21:41:27 +0200

    add info in ranking Sex&Money,Bitch!

--- src/sections/ranking/Ranking.jsx
+++ src/sections/ranking/Ranking.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ranking from './ranking'
 
 const Ranking = () => {
+
 	return (
 		<div>
 			<h1>Ranking!</h1>
+			<div>{ranking.info.map((res) => {
+				console.log(res)
+				return <div >
+					<div > <h5>Sexo: </h5> {res.Sexo} </div>
+					<div > <h5> Dinero: </h5> {res.Dinero}</div>
+				</div>
+			})}
+			</div>
 		</div>
 	);
 };
