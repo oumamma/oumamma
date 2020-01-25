33c23df4c26ea2aa9859fcfd8b3f88c9a6524b18
Francisco Novella Fletcher <fran.novella@demium.com>
2020-01-25 14:09:22 +0100

    Ranking desktop/tablet stable

--- src/sections/ranking/Ranking.jsx
+++ src/sections/ranking/Ranking.jsx
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import fetch from 'node-fetch';
 import './Ranking.scss';
 import SpreadsheetService from '../../services/spreadsheets/SpreadsheetService';
+import Loading from '../Loading';
 
 const Ranking = () => {
+	const [loading, setLoading] = useState(true);
 	const [money, setMoney] = useState([]);
 	const [fuck, setFuck] = useState([]);
 
 	useEffect(() => {
 		SpreadsheetService.getRanking().then(res => {
 			let moneyList = [...res];
-			moneyList = moneyList.sort((a, b) => (+a.Dinero > +b.Dinero ? 1 : -1));
+			moneyList = moneyList.sort((a, b) => (+a.Dinero < +b.Dinero ? 1 : -1));
 			setMoney(moneyList);
 
 			let fuckList = [...res];
-			fuckList = fuckList.sort((a, b) => (+a.Sexo > +b.Sexo ? 1 : -1));
+			fuckList = fuckList.sort((a, b) => (+a.Sexo < +b.Sexo ? 1 : -1));
 			setFuck(fuckList);
-			return res;
+
+			setLoading(false);
 		});
 	}, []);
 
@@ -25,47 +27,59 @@ const Ranking = () => {
 			<div className="toiler-paper-container">
 				<div className="left-toilet-paper-bg">
 					<div className="left-toilet-paper-content">
-						<div className="img-container">
-							<img src="/money.png"></img>
-						</div>
-						<div className="ranking-list">
-							{money.map((result, indx) => (
-								<div
-									className="ranking-list-element"
-									key={result['Nombre red social']}
-								>
-									<div className="ranking-list-element-position">
-										{indx + 1}
-									</div>
-									<div className="ranking-list-element-name">
-										{result['Nombre red social']}
-									</div>
+						{loading ? (
+							<Loading></Loading>
+						) : (
+							<>
+								<div className="img-container">
+									<img src="/money.png"></img>
 								</div>
-							))}
-						</div>
+								<div className="ranking-list">
+									{money.map((result, index) => (
+										<div
+											className="ranking-list-element"
+											key={`${index}${result['Nombre red social']}`}
+										>
+											<div className="ranking-list-element-position">
+												{index + 1}
+											</div>
+											<div className="ranking-list-element-name">
+												{result['Nombre red social']}
+											</div>
+										</div>
+									))}
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 				<div className="center"></div>
 				<div className="right-toilet-paper-bg">
 					<div className="right-toilet-paper-content">
-						<div className="img-container">
-							<img src="/fuck.png"></img>
-						</div>
-						<div className="ranking-list">
-							{fuck.map((result, indx) => (
-								<div
-									className="ranking-list-element"
-									key={result['Nombre red social']}
-								>
-									<div className="ranking-list-element-position">
-										{indx + 1}
-									</div>
-									<div className="ranking-list-element-name">
-										{result['Nombre red social']}
-									</div>
+						{loading ? (
+							<Loading></Loading>
+						) : (
+							<>
+								<div className="img-container">
+									<img src="/fuck.png"></img>
 								</div>
-							))}
-						</div>
+								<div className="ranking-list">
+									{fuck.map((result, index) => (
+										<div
+											className="ranking-list-element"
+											key={`${index}${result['Nombre red social']}`}
+										>
+											<div className="ranking-list-element-position">
+												{index + 1}
+											</div>
+											<div className="ranking-list-element-name">
+												{result['Nombre red social']}
+											</div>
+										</div>
+									))}
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 			</div>
