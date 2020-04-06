a1b14b568c007e17f1c94b58470d926b9ad469dd
Francisco Novella Fletcher <fran.novella@demium.com>
2020-04-07 01:18:08 +0200

    fix updateRow headers

--- src/services/spreadsheets/SpreadsheetService.js
+++ src/services/spreadsheets/SpreadsheetService.js
@@ -29,11 +29,9 @@ class SpreadsheetService {
 		return fetch(this.host, {
 			method: 'POST', // or 'PUT'
 			body: JSON.stringify(data),
-			headers: {
-				'Content-Type': 'application/json',
-			},
 			mode: 'cors',
 			headers: {
+				'Content-Type': 'application/json',
 				'Access-Control-Allow-Origin': '*',
 				'Access-Control-Allow-Headers':
 					'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
